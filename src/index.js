9a213cbc29cb7eb7ea68ad51be2f5c6ed123555a
Jackie Johnston <jackie.johnston@pnmac.com>
2017-08-30 16:18:02 -0700

    feat: Add redux-logger

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import logger from 'redux-logger'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
 )
 // console.log(store.getState())
 
